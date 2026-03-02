b3efe6b037bdf451bd70dc9f89b490ed03d6b498
Manya Sharma <manya.s1008@gmail.com>
2026-03-02 10:17:16 +0530

    UC6: Queue + Stack Based Palindrome Check

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
@@ -5,19 +8,27 @@ public class PalindromeCheckerApp {
         // Hardcoded string (String Literal)
         String word = "madam";
 
-        // Create a Stack to store characters
+        // Create Stack (LIFO)
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Enqueue and Push characters
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO (Enqueue)
         }
 
         boolean isPalindrome = true;
 
-        // Pop characters from stack and compare
+        // Compare dequeue (queue) with pop (stack)
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
+            char fromQueue = queue.remove();  // Dequeue (FIFO)
+            char fromStack = stack.pop();     // Pop (LIFO)
+
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
@@ -32,11 +43,4 @@ public class PalindromeCheckerApp {
     }
 }
 
-        // Display result
-        if (isPalindrome) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is NOT a Palindrome.");
-        }
-    }
-}
+
